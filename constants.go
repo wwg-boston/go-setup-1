ee3185ee58da7e1314774a3bde6025eb66e89e2d
Alexandra Mirtcheva <amirtcheva@gmail.com>
2015-10-18 16:27:53 -0400

    iota and use of constants

--- constants.go
+++ constants.go
@@ -1,9 +1,12 @@
 package main
 
 // iota gives us an auto-incrementing number that determines
-// the value of the integers 
+// the value of the integers (basic type of enumeration)
+// first = 1
+// second = 1 * 2 ^ 10 = 1024
+// third = second * 2 ^ 10 = 1024 * 1024 = 1048576
 const (
-    first = iota
+    first =  1 << (10 * iota)
     second
     third
 )
