615fe2596bb067d744e7d95f8874f5553fdd71c5
Alexandra Mirtcheva <amirtcheva@gmail.com>
2015-10-18 17:55:18 -0400

    named return values

--- functions.go
+++ functions.go
@@ -4,7 +4,7 @@ func main() {
   name := "James"
   printGreeting("one", "two", "three")
   println(name);
-  total, result := add(1, 2, 3)
+  total, result := add2(1, 2, 3)
   println("There are: ", total, " items, and the sum is: ", result)
 }
 
@@ -35,4 +35,13 @@ func add(items ...int) (int, int) {
       result += item
   }
   return len(items), result
+}
+
+// name return variables
+func add2(items ...int) (total int, result int) {
+  for _, item := range items{
+      result += item
+  }
+  total = len(items)
+  return
 }
