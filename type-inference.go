671d45d6437a3d3b10dc3f5da531344d7733df29
Alexandra Mirtcheva <amirtcheva@gmail.com>
2015-10-19 10:37:13 -0400

    type inference in go

---
+++ type-inference.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  var x = 2
+  y := 1
+  z := 1.5
+  g := "1"
+  fmt.Printf("y is of type %T\n", y)
+  fmt.Printf("z is of type %T\n", z)
+  fmt.Printf("x is of type %T\n", x)
+  fmt.Printf("g is of type %T\n", g)
+}
